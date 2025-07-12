1b4cd214d46a01fa245e7c836d77e51a435f5596
fluffb4ll <real.fluffball@gmail.com>
2025-07-12 16:53:20 +0500

    WIP: Link Shortener frontend

--- src/main/resources/static/scripts/ls.js
+++ src/main/resources/static/scripts/ls.js
@@ -5,8 +5,29 @@ async function compressLink() {
 
     const response = await fetch("api/ls", {
         method:"POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({link: link})
+        body: link
     });
 
+    if (!response.ok) {
+        alert('Response status: ' + response.status);
+    } else {
+        const shortLink = window.location.href
+                                .replace("http://", "")
+                                .replace('https://', "")
+                                .replace('?', '')
+                                + "/" + await response.text();
+        if (document.getElementById('shortLinkContainer') == null) {
+            let linkForUser = document.createElement('p');
+            linkForUser.id = 'shortLink';
+            linkForUser.innerText = shortLink;
+
+            let linkContainer = document.createElement('div');
+            linkContainer.id = "shortLinkContainer";
+            linkContainer.appendChild(linkForUser);
+
+            document.getElementById('mainWindow').appendChild(linkContainer);
+        } else {
+            document.getElementById('shortLink').innerText = shortLink;
+        }
+    }
 }
