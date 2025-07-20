407ea5b5e13a24c0aac8ced23a7050eceed9ff47
fluffb4ll <real.fluffball@gmail.com>
2025-07-20 15:51:09 +0500

    WIP: Link Shortener frontend

--- src/main/resources/static/scripts/ls.js
+++ src/main/resources/static/scripts/ls.js
@@ -1,4 +1,10 @@
-document.getElementById("confirm").addEventListener("click", compressLink);
+const confirm = document.getElementById("confirm");
+const link = document.getElementById("link");
+confirm.addEventListener("click", compressLink);
+
+link.addEventListener("input", () => {
+    confirm.disabled = link.value.length == 0;
+});
 
 async function compressLink() {
     const link = document.getElementById('link').value;
@@ -17,17 +23,68 @@ async function compressLink() {
                                 .replace('?', '')
                                 + "/" + await response.text();
         if (document.getElementById('shortLinkContainer') == null) {
-            let linkForUser = document.createElement('p');
-            linkForUser.id = 'shortLink';
-            linkForUser.innerText = shortLink;
+            let linkForUser = document.createElement('a');
+            linkForUser.id = "shortLink"
+            linkForUser.href = shortLink;
+
+            let linkText = document.createElement('span');
+            linkText.id = 'shortLinkText';
+            linkText.innerText = shortLink;
+
+            linkForUser.appendChild(linkText);
 
             let linkContainer = document.createElement('div');
             linkContainer.id = "shortLinkContainer";
             linkContainer.appendChild(linkForUser);
 
-            document.getElementById('mainWindow').appendChild(linkContainer);
+            let copyButton = document.createElement('button');
+            copyButton.type = "button";
+            copyButton.id = "copyButton";
+            copyButton.addEventListener("click", copyLink);
+
+            let buttonName = document.createElement('span');
+            buttonName.id = "copyButtonName"
+            buttonName.innerText = "copy"
+
+            let tooltip = document.createElement('span');
+            tooltip.id = "copyTooltip";
+            tooltip.className = "tooltiptext";
+            tooltip.innerText = "copied :D";
+
+            copyButton.appendChild(buttonName);
+            copyButton.appendChild(tooltip);
+
+            let buttonContainer = document.createElement('div');
+            buttonContainer.id = 'buttonContainer';
+            buttonContainer.className = "tooltip"
+            buttonContainer.appendChild(copyButton);
+
+            let linkCard = document.createElement('div');
+            linkCard.id = "shortLinkCard";
+            linkCard.appendChild(linkContainer);
+            linkCard.appendChild(buttonContainer);
+
+            document.getElementById('mainWindow').appendChild(linkCard);
         } else {
-            document.getElementById('shortLink').innerText = shortLink;
+            document.getElementById('shortLinkText').innerText = shortLink;
+            document.getElementById('shortLink').href = shortLink;
         }
     }
+}
+
+function copyLink() {
+    let shortLink = document.getElementById('shortLink');
+    const tooltip = document.getElementById('copyTooltip');
+    navigator.clipboard.writeText(shortLink.innerText);
+    tooltip.visibility = "visible";
+    tooltip.opacity = 1;
+    delay(2000);
+    tooltip.visibility = "hidden";
+    tooltip.opacity = 0;
+}
+
+function delay(time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
 }
